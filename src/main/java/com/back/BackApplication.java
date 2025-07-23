e68707dd6b72d1fbd9abeb1e71ff243357ef58d5
user.email <younsoo6103@naver.com>
2025-07-23 13:37:41 +0900

    [chore] 기본 설정

--- src/main/java/com/back/BackApplication.java
+++ src/main/java/com/back/BackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Team10Application {
+public class BackApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(Team10Application.class, args);
+        SpringApplication.run(BackApplication.class, args);
     }
 
 }
