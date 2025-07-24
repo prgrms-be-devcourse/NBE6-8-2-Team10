5be2385bda60a7bc4c5025b82accd07c7baf7b76
석희성 <dcs1206@naver.com>
2025-07-24 17:59:54 +0900

    [feat] SpringSecurity 설정 (#58)
    * [feat] SpringSecurity 설정
    
    * [feat] SpringSecurity 설정

--- src/main/java/com/back/global/security/config/SecurityConfig.java
+++ src/main/java/com/back/global/security/config/SecurityConfig.java
@@ -33,10 +33,15 @@ public class SecurityConfig {
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/auth/**", "/h2-console/**").permitAll() // 운영 환경에서는 보안 위험이 될 수 있음 -> 추후 보완 필요
                         //.requestMatchers("/auth/**").permitAll()
+                        .requestMatchers("/*.html").permitAll()  // HTML 파일 접근 허용
+                        .requestMatchers("/chat/**").permitAll()     // WebSocket 엔드포인트 허용
+                        .requestMatchers("/topic/**").permitAll()    // STOMP 구독 경로 허용
+                        .requestMatchers("/app/**").permitAll()      // 메시지 전송 경로 허용
                         .anyRequest().authenticated()
                 )
                 .addFilterBefore(new JwtFilter(jwtTokenProvider, userDetailsService), UsernamePasswordAuthenticationFilter.class)
                 .headers(headers -> headers.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable)); // H2 콘솔 접근 허용
+
 
         return http.build();
     }
