27f1e328a4d29b3a02ec53746c28b0a58a9eeb01
석희성 <dcs1206@naver.com>
2025-07-24 23:44:31 +0900

    Feat#60 (#61)
    * [feat] ChatService 구현
    
    * [feat] ChatService 구현

--- src/main/java/com/back/domain/chat/chat/controller/ChatController.java
+++ src/main/java/com/back/domain/chat/chat/controller/ChatController.java
@@ -1,17 +1,23 @@
 package com.back.domain.chat.chat.controller;
 
 import com.back.domain.chat.chat.dto.MessageDto;
+import com.back.domain.chat.chat.service.ChatService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 @Controller
+@RequiredArgsConstructor
 public class ChatController {
+
+    private final ChatService chatService;
+
 
     @MessageMapping("/sendMessage")
     @SendTo("/topic/messages")
     public MessageDto sendMessage(MessageDto chatMessage) {
-
+        chatService.saveMessage(chatMessage);
         return chatMessage;
     }
 }
---
+++ src/main/java/com/back/domain/chat/chat/service/ChatService.java
@@ -0,0 +1,12 @@
+package com.back.domain.chat.chat.service;
+
+import com.back.domain.chat.chat.dto.MessageDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ChatService {
+
+    public void saveMessage(MessageDto chatMessage) {
+
+    }
+}
