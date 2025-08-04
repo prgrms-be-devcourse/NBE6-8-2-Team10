ade1c309c62f968841b5fee53821e70c4b52f8e0
NewplayerKOR <149351553+NewplayerKOR@users.noreply.github.com>
2025-08-04 12:06:56 +0900

    [feat] : FileDownloadController 받아오는 url 수정 (#213)
    * [feat] : FileDownloadController 받아오는 url 수정
    
    * [feat] : FileDownloadController 받아오는 url 수정

--- src/main/java/com/back/domain/files/files/controller/FileDownloadController.java
+++ src/main/java/com/back/domain/files/files/controller/FileDownloadController.java
@@ -1,17 +1,16 @@
 package com.back.domain.files.files.controller;
 
 import com.back.domain.files.files.service.FileStorageService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 
 @RestController
@@ -22,9 +21,14 @@ public class FileDownloadController {
     private final FileStorageService fileStorageService;
 
     // 파일 다운로드 API
-    @GetMapping("/{subFolder}/{fileName:.+}")
-    public ResponseEntity<Resource> downloadFile(@PathVariable String subFolder, @PathVariable String fileName, HttpServletRequest request) {
-        String fileUrl = "/files/" + subFolder + "/" + fileName;
+    @GetMapping("/**")
+    public ResponseEntity<Resource> downloadFile(HttpServletRequest request) {
+        String fileUrl = request.getRequestURI();
+        // 경로 순회 공격 방지
+        if (fileUrl.contains("..") || fileUrl.contains("./") || fileUrl.contains("\\")) {
+            throw new IllegalArgumentException("Invalid file URL: " + fileUrl);
+        }
+
         Resource resource = fileStorageService.loadFileAsResource(fileUrl);
 
         String contentType = null;
